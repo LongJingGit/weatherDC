a9f96436dfbf9c3ff51086fe80d4c072f6a3c947
JINGLONGGIT <jinglong_email@163.com>
2020-04-22 22:25:20 +0800

    [新增] 新增通过OCI封装库将数据保存到数据库中的模块

---
+++ code/dbop.cpp
@@ -0,0 +1,113 @@
+/*********************************************************************************
+ * File Name: dbop.cpp
+ * Description: 通过OCI封装库将数据信息保存到数据库中
+ * Author: jinglong
+ * Date: 2020年4月22日 7:00
+ * History: 
+ *********************************************************************************/
+
+#include "dbop.h"
+
+extern CLogManager* logFile;
+
+CDataSaveTable::CDataSaveTable()
+{
+
+}
+
+CDataSaveTable::~CDataSaveTable()
+{
+
+}
+
+int CDataSaveTable::initDB(char * strUserName, char * strUserPwd, char * strTnsName, CORADBManager & oracleManager)
+{
+    if (0 != oracleManager.initDB())
+    {
+        printf("initDB failed\n");
+        return -1;
+    }
+
+    if (0 != oracleManager.SingleConnectDB(strUserName, strUserPwd, strTnsName, "AMERICAN_AMERICA.WE8MSWIN1252", 0))
+    {
+        printf("connectDB failed\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+int CDataSaveTable::getConnection(char * sqlbuf, CORADBManager & oracleManager, CORADBSqlStmt & sql)
+{
+    oracleManager.getConnection(&sql);
+    sql.initSQLStmt();
+
+    if (sql.prepareSQL(sqlbuf) != 0)
+    {
+        printf("准备SQL语句失败\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+// 插入数据
+/*数据库表结构
+create table weatherdc
+(
+    cObtid          varchar2(10),
+    cDatatime       date,
+    iTemperature    number(4),
+    iAirp           number(6),
+    ihumidity       number(4),
+    iWindDirec      number(4),
+    iWindSpeed      number(4),
+    iRainFall       number(4),
+    iVisibility     number(7),
+    primary key (cObtid, cDatatime)
+);
+*/
+int CDataSaveTable::inserttable(CORADBManager &oracleManager)
+{
+    char sqlbuf[2048] = {"insert into weatherdc(cObtid, cDatatime, iTemperature, iAirp, ihumidity, iWindDirec, iWindSpeed, iRainFall, iVisibility) \ 
+        values(:1, to_date(:2, 'yyyy-mm-dd hh24:mi:ss'), :3, :4, :5, :6, :7, :8, :9)"};
+
+    CORADBSqlStmt sql;
+
+    if (getConnection(sqlbuf, oracleManager, sql) != 0)
+    {
+        return -1;
+    }
+
+    if (sql.bindIN(1, m_sDataInfo.cObtid, sizeof(m_sDataInfo.cObtid) - 1) != 0 
+      || sql.bindIN(2, m_sDataInfo.cDatatime, sizeof(m_sDataInfo.cDatatime) - 1) != 0 
+      || sql.bindIN(3, &m_sDataInfo.iTemperature) != 0 
+      || sql.bindIN(4, &m_sDataInfo.iAirp) != 0 
+      || sql.bindIN(5, &m_sDataInfo.ihumidity) != 0 
+      || sql.bindIN(6, &m_sDataInfo.iWindDirec) != 0
+      || sql.bindIN(7, &m_sDataInfo.iWindSpeed) != 0
+      || sql.bindIN(8, &m_sDataInfo.iRainFall) != 0
+      || sql.bindIN(9, &m_sDataInfo.iVisibility) != 0)
+    {
+        printf("绑定失败\n");
+        return -1;
+    }
+
+    if (sql.execSQL() != 0)
+    {
+        printf("执行SQL语句失败\n");
+        return -1;
+    }
+
+    if (sql.commit() != 0)
+    {
+        printf("提交失败\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+
+
+
---
+++ code/dbop.h
@@ -0,0 +1,44 @@
+#ifndef __DBOP_H_
+#define __DBOP_H_
+
+#include "oradbmanager.h"
+#include "logManager.h"
+
+// 分钟观测数据数据结构
+struct st_surfdata
+{
+    char cObtid[64];             // 站点代码
+    char cDatatime[128];         // 数据时间(格式: yyyy-mm-dd hh:mi:ss)
+    int iTemperature;            // 温度(0.1摄氏度: 保留一位小数)
+    int iAirp;                   // 气压(0.1百帕: 保留一位小数)
+    int ihumidity;               // 相对湿度(0-100之间的值)
+    int iWindDirec;              // 风向(0-360之间的值)
+    int iWindSpeed;              // 风速(0.1m/s)
+    int iRainFall;               // 降雨量(0.1mm)
+    int iVisibility;             // 能见度(0.1m)
+};
+
+// 数据存储操作类
+class CDataSaveTable
+{
+public:
+    CDataSaveTable();
+    ~CDataSaveTable();
+
+private:
+    int getConnection(char *sqlbuf, CORADBManager &oracleManager, CORADBSqlStmt &sql);
+
+public:
+    int initDB(char * strUserName, char * strUserPwd, char * strTnsName, CORADBManager &oracleManager);
+    int inserttable(CORADBManager &oracleManager);
+    int deletetable(CORADBManager &oracleManager);
+    int selecttable(CORADBManager &oracleManager);
+    int updatetable(CORADBManager &oracleManager);
+
+public:
+    struct st_surfdata m_sDataInfo;
+};
+
+#endif
+
+
